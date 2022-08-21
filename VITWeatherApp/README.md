e6266cd69191ee16a3eff9ba48383fbb83629d09
AkshajRaut <akshajraut@gmail.com>
2022-08-21 10:41:02 +0530

    Update

--- VITWeatherApp/README.md
+++
@@ -1,4 +0,0 @@
-# VIT Weather Station
-
-A weather app made for monitoring the Weather conditions of Vidyalankar College Campus.
-
--- VITWeatherApp/android/app/src/main/kotlin/com/vidyalankar/weatherstation/vitweather/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.vidyalankar.weatherstation.vitweather
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
